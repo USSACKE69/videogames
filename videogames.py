a8263c3fc32285795c336d68374e89171f1f8435
issac Moto <ussac_123@gmail.com>
2024-10-07 23:09:55 -0600

    Conversión de la columna 'user_score' a tipo float y reemplazo de 'tbd' con NaN

--- videogames.py
+++ videogames.py
@@ -14,3 +14,24 @@ df.columns = df.columns.str.lower()
 # Verificar las columnas después de la modificación
 print("Columnas modificadas:")
 print(df.columns)
+
+# Verificar los tipos de datos actuales
+print("Tipos de datos originales:")
+print(df.dtypes)
+
+# Ver los valores únicos de la columna 'user_score'
+print("Valores únicos en 'user_score':")
+print(df['user_score'].unique())
+    
+              # Reemplazar el valor 'tbd' con NaN
+df['user_score'] = df['user_score'].replace('tbd', pd.NA)
+
+            # Convertir 'user_score' a tipo float
+df['user_score'] = pd.to_numeric(df['user_score'], errors='coerce')
+
+# Verificar la conversión
+print("Tipos de datos después de la conversión de 'user_score':")
+print(df['user_score'].dtypes)
+
+# Opcional: Ver algunos valores para confirmar la conversión
+print(df['user_score'].head())
