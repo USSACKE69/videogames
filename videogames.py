8df2fd01d576f56ad239b32e95efa9469301b84d
issac Moto <ussac_123@gmail.com>
2024-10-09 21:25:18 -0600

    Análisis de lanzamientos de juegos por año y preparación de los datos para análisis.

--- videogames.py
+++ videogames.py
@@ -57,3 +57,39 @@ print(df['year_of_release'].unique())
 # Ver las primeras filas para confirmar
 print("Primeros valores en 'year_of_release' después de la imputación:")
 print(df['year_of_release'].head(10))
+
+                # Verificar si hay valores faltantes en otras columnas
+print(df.isnull().sum())
+print(df.dtypes)
+# Eliminar filas con valores faltantes en 'name' y 'genre'
+df = df.dropna(subset=['name', 'genre'])
+
+# Imputar 'critic_score' y 'user_score' con la mediana
+df['critic_score'] = df['critic_score'].fillna(df['critic_score'].median())
+df['user_score'] = df['user_score'].fillna(df['user_score'].median())
+
+# Imputar 'rating' con el valor más frecuente (moda)
+df['rating'] = df['rating'].fillna(df['rating'].mode()[0])
+
+# Verificar los cambios
+print(df.isnull().sum())
+
+from sklearn.preprocessing import MinMaxScaler
+
+scaler = MinMaxScaler()
+df[['na_sales', 'eu_sales', 'jp_sales', 'other_sales']] = scaler.fit_transform(df[['na_sales', 'eu_sales', 'jp_sales', 'other_sales']])
+
+# Verificar si hay outliers en las columnas de ventas
+df[['na_sales', 'eu_sales', 'jp_sales', 'other_sales']].describe()
+
+df = pd.get_dummies(df, columns=['platform'], drop_first=True)
+
+# Calcular las ventas totales sumando las ventas en todas las regiones
+df['total_sales'] = df['na_sales'] + df['eu_sales'] + df['jp_sales'] + df['other_sales']
+
+# Verificar el resultado mostrando las primeras filas
+print(df[['name', 'na_sales', 'eu_sales', 'jp_sales', 'other_sales', 'total_sales']].head())
+
+# Guardar el DataFrame depurado en un archivo CSV
+df.to_csv('/Users/issacmoto/Desktop/Tripletent/Proyectos Tripletent/Myprojectvideogames/videogames/videogames_cleaned.csv', index=False)
+
